c25599a4491af866ad07c8a6d6d3b15370bb0eb9
D3kion <dekion.dev@gmail.com>
2023-12-25 21:47:31 +0300

    feat(api): some api structure

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { LinksModule } from './links/links.module';
+import { ProjectsModule } from './projects/projects.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, LinksModule, ProjectsModule],
   controllers: [AppController],
   providers: [AppService],
 })
